034edeeb22b3807625721eb0ff1792a4cff36040
Kotaro Hashimoto <kotaro.hashimoto.jv@hitachi.com>
2016-10-31 22:18:00 +0900

    hoge
    Signed-off-by: Kotaro Hashimoto <kotaro.hashimoto.jv@hitachi.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foo::Application.config.secret_key_base = 'd867183aa17460313976447ec1a0a73393dfcd6131b1b37d3bb57db415a4dced22a8aa589ca953bb18a216e834d373c71fb1536a863e3a6804be62ba63ed769f'
